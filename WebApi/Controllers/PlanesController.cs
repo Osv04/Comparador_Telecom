ece3e925756993d691f4259b65737ac8030cd7e3
Osv04 <oscar.veloz4@gmail.com>
2021-10-20 20:25:54 -0400

    Added 2 proyects Web API and MVC, then link with db and creation fo controllers inside the Web Api proyect

---
+++ WebApi/Controllers/PlanesController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class PlanesController : ApiController
+    {
+        private ProyectoArquictecturaEntities db = new ProyectoArquictecturaEntities();
+
+        // GET: api/Planes
+        public IQueryable<Plane> GetPlanes()
+        {
+            return db.Planes;
+        }
+
+        // GET: api/Planes/5
+        [ResponseType(typeof(Plane))]
+        public IHttpActionResult GetPlane(int id)
+        {
+            Plane plane = db.Planes.Find(id);
+            if (plane == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(plane);
+        }
+
+        // PUT: api/Planes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPlane(int id, Plane plane)
+        {
+
+            if (id != plane.IdPlan)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(plane).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlaneExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Planes
+        [ResponseType(typeof(Plane))]
+        public IHttpActionResult PostPlane(Plane plane)
+        {
+            if (!ModelState.IsValid)
+
+            db.Planes.Add(plane);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (PlaneExists(plane.IdPlan))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = plane.IdPlan }, plane);
+        }
+
+        // DELETE: api/Planes/5
+        [ResponseType(typeof(Plane))]
+        public IHttpActionResult DeletePlane(int id)
+        {
+            Plane plane = db.Planes.Find(id);
+            if (plane == null)
+            {
+                return NotFound();
+            }
+
+            db.Planes.Remove(plane);
+            db.SaveChanges();
+
+            return Ok(plane);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PlaneExists(int id)
+        {
+            return db.Planes.Count(e => e.IdPlan == id) > 0;
+        }
+    }
+}
