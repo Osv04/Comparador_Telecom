85ddfb1132aaac6052c996883b0e48dc890740a8
Jean C. Mendoza <73866099+MenJCP@users.noreply.github.com>
2021-10-24 12:29:02 -0400

    Create operation added for Planes Controller, added funtional view for create planes

--- Mvc/Models/mvcPlanesModel.cs
+++ Mvc/Models/mvcPlanesModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using WebApi.Models;
@@ -9,11 +10,18 @@ namespace Mvc.Models
     public class mvcPlanesModel
     {
         public int IdPlan { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
         public string NombrePlan { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
         public string Decripcion { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
         public Nullable<decimal> Precio { get; set; }
         public Nullable<int> IdEmpresa { get; set; }
 
+        [Required(ErrorMessage = "This field is required")]
         public virtual Empresa Empresa { get; set; }
     }
 }
